effee267532608920afa3d0a307ab36ac0ea6120
Sage Fremont <36422856+SageWare@users.noreply.github.com>
2018-04-06 19:29:22 -0400

    storing xyther.js

---
+++ scripts/xyther.js
@@ -0,0 +1,12 @@
+<div
+  style="height:350; background:transparent"
+  onmousedown="osc.start(); osc2.start()"
+  onmousemove="osc.frequency.value = event.clientY;this.innerText=event.clientY; osc2.frequency.value = event.clientX;this.innerText=event.clientX"
+  >XY Theramin. 2017. Sage Fremont.  CLICK ANYWHERE TO START.</div>
+<script>
+    var AC = window.AudioContext || window.webkitAudioContext;
+    var actx = new AC();
+    var osc = actx.createOscillator(); osc.connect(actx.destination);
+    var actx2 = new AC();
+    var osc2 = actx2.createOscillator(); osc2.connect(actx2.destination);
+</script>
